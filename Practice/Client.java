501c504f6550073e9a6849e4d750a1084328517b
Subhadip Das <subhadipdas911@gmail.com>
2024-10-14 11:08:31 +0530

    We cannot change Final Data Member even via Class Name

--- Practice/Client.java
+++ Practice/Client.java
@@ -2,15 +2,19 @@ package Practice;
 
 public class Client {
 	public static void main(String[] args) {
-		A o1 = new A();
+//		A o1 = new A();
+//		
+//		A o2 = new A();
+//		
+//		System.out.println("Coming from O1 "+o1.piVal);
+//		System.out.println("Coming from O2 "+o2.piVal);
+//		
+//		o1.piVal = 12.333412;
+//		
+//		System.out.println("Updated piVal via o1 "+o1.piVal);
 		
-		A o2 = new A();
+		A.piVal = 12.12380123;
 		
-		System.out.println("Coming from O1 "+o1.piVal);
-		System.out.println("Coming from O2 "+o2.piVal);
-		
-		o1.piVal = 12.333412;
-		
-		System.out.println("Updated piVal via o1 "+o1.piVal);
+		System.out.println(A.piVal);
 	}
 }
