381cd22067693a815add1da7a0f66c0be0f4e4e6
Subhadip Das <subhadipdas911@gmail.com>
2024-10-18 09:31:39 +0530

    Usage of SuperKeyword in Constructor Chanining

--- Practice/B.java
+++ Practice/B.java
@@ -1,5 +1,10 @@
 package Practice;
 
-public class B {
-	
+public class B extends A{
+	int d2;
+	B(int d2)
+	{
+		super();
+		this.d2 = d2;
+	}
 }
--- Practice/Client.java
+++ Practice/Client.java
@@ -3,7 +3,9 @@ package Practice;
 public class Client {
 
 	public static void main(String[] args) {
-
+		B ob = new B(24);
+		System.out.println(ob.d1);
+		System.out.println(ob.d2);
 	}
 
 }
