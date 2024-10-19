79d4d1c345a299c6940d9eac017546a7258be874
Subhadip Das <subhadipdas911@gmail.com>
2024-10-19 19:56:23 +0530

    Using StringName.replace('oldChar','newChar') will create a new instance and a new Char []

--- String_Interview_Question_Part_1/Client.java
+++ String_Interview_Question_Part_1/Client.java
@@ -3,10 +3,15 @@ package String_Interview_Question_Part_1;
 public class Client {
 
 	public static void main(String[] args) {
-		String s1 = "hello";
-		String s2 = s1;
+//		String s1 = "hello";
+//		String s2 = s1;
+//		
+//		s1 = "bye";
 		
-		s1 = "bye";
+//Example : 2
+		
+		String s1 = "hello";
+		String s2 = s1.replace('h', 'x');
 		
 		
 
