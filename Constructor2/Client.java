f09b498b73fe7adebaecd0dccfd09585e481d1e2
Subhadip Das <subhadipdas911@gmail.com>
2024-11-14 11:26:36 +0530

    Parameterized and Default Constructor invocation

---
+++ Constructor2/Client.java
@@ -0,0 +1,12 @@
+package Constructor2;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Constructor c = new Constructor(); //I am the Constructor [Default Constructor]
+		
+		Constructor c1 = new Constructor(4,5); // I am inside Parameterized Constructor [Parameterized Constructor]
+	}
+
+}
