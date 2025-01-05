a37dccb439ce5d9e5700d9326ce18bedd3f31ff2
Subhadip Das <subhadipdas911@gmail.com>
2025-01-05 20:16:55 +0530

    Used Mutex to provide locks in the Critical section of the Code

--- MultiThreading/Synchronization/Mutex/Subtract.java
+++ MultiThreading/Synchronization/Mutex/Subtract.java
@@ -18,7 +18,7 @@ public class Subtract implements Callable<Void> {
 
 		for (int i = 1; i <= 10000; i++) {
 			lock.lock();
-			this.count.value -= i;
+			this.count.value -= i;// this is the part of the code where shared object is involved between threads.
 			lock.unlock();
 		}
 
