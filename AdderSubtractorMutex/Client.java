4e8532fb646b3cd9703e2c01e985d2aa9af56d78
Subhadip Das <subhadipdas911@gmail.com>
2025-01-06 22:10:52 +0530

    Added the concept of why Shared lock is used instead of 2 different locks

--- AdderSubtractorMutex/Client.java
+++ AdderSubtractorMutex/Client.java
@@ -7,6 +7,16 @@ import java.util.concurrent.Future;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/*
+ Shared lock basically means the following :-
+ If there is a room and there are 5 doors. All the doors have the same lock. (Meaning all the doors can be opened by only one TYPE of LOCK) 
+ By using Shared lock concept, we are sending only one KEY.
+ 
+ If we would have created more than one key and send one KEY to thread1 and another key to thread2.
+ That is basically like sending 2 keys of the SAME TYPE.
+ Meaning if one thread has locked the Critical section of the code and hasn't unlocked it yet.
+ Another thread can unlock the Critical section using the 2nd key we passed.
+ */
 public class Client {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
