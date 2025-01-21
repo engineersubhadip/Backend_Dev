b2272dbdbf8346a9dfe4be89cdea32956f05c709
Subhadip Das <subhadipdas911@gmail.com>
2025-01-21 14:13:30 +0530

    Updated as to why V1.V2 and V3 are not singletons

--- src/main/java/SingletonDesignPattern/Client.java
+++ src/main/java/SingletonDesignPattern/Client.java
@@ -1,64 +1,38 @@
 package SingletonDesignPattern;
 
-import System.out.SingletonDesign_V4;
-
 public class Client {
 
 	public static void main(String[] args) {
-
+		
 		/*
-		 * Not a singleton as multiple objects are getting created :-
-		 * 
-		 * SingletonDesign_V1 ob1 = new SingletonDesign_V1("a", "k", 23);
-		 * 
-		 * SingletonDesign_V1 ob2 = new SingletonDesign_V1("b", "k", 24);
+//		Version 1 :-
+		
+		SingletonDesign_V1 v1 = new SingletonDesign_V1("A","Mizoram",23);
+		SingletonDesign_V1 v2 = new SingletonDesign_V1("B","Aizwal",28);
+		
+//		Since we are able to create multiple objects of the Class.
+//		This is not a singleton.
 		 */
-
+		
 		/*
-		 * Not a singleton as multiple objects are getting created :-
-		 * 
-		 * SingletonDesign_V2 ob1 = new SingletonDesign_V2();
-		 * 
-		 * SingletonDesign_V2 ob2 = new SingletonDesign_V2();
-		 * 
-		 * System.out.println(ob1);
-		 * 
-		 * System.out.println(ob2);
-		 * 
+//		Version 2:-
+		SingletonDesign_V2 v1 = new SingletonDesign_V2();
+		SingletonDesign_V2 v2 = new SingletonDesign_V2();
+		
+//		Still we are able to create multiple objects because default constructor is still there.
+//		This is not a singleton.
+///
 		 */
-
+		
 		/*
-		 * Not a singleton as more than 1 object is getting created
-		 * 
-		 * SingletonDesign_V3 ob1 = SingletonDesign_V3.setObject();
-		 * 
-		 * // System.out.println(ob1.name); // System.out.println(ob1.city); //
-		 * System.out.println(ob1.age);
-		 * 
-		 * SingletonDesign_V3 ob2 = SingletonDesign_V3.setObject();
-		 * 
-		 * System.out.println(ob1); System.out.println(ob2);
-		 * 
-		 * "obj" is a local variable.
-		 * 
-		 */
-
-//		Version 4 :-
-
-		SingletonDesign_V4 ob1 = SingletonDesign_V4.setObject();
-
-		System.out.println(ob1);
-
-		SingletonDesign_V4 ob2 = SingletonDesign_V4.setObject();
-
-		System.out.println(ob2);
-
-		/*
-		 * What if we put the entire if statement inside the constructor and make the
-		 * constructor public and remove the setObject() altogether ? Will this work ?
-		 * 
-		 * NO, because constructor does not have a return type
-		 */
+//		Version 3 :-
+		SingletonDesign_V3 v1 = SingletonDesign_V3.setObject("A","Mizoram",23);
+		SingletonDesign_V3 v2 = SingletonDesign_V3.setObject("B","Imphal",25);
+		
+//		Still we are able to create multiple objects.
+//		This is not a single ton.		*/
+		
+		
 	}
 
 }
