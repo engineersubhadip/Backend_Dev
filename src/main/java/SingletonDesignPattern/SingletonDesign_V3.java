8aaf12ee75e3b00707a9f90c827dfebf6acdd117
Subhadip Das <subhadipdas911@gmail.com>
2025-01-21 14:13:55 +0530

    Refactor : Refactored the Code

--- src/main/java/SingletonDesignPattern/SingletonDesign_V3.java
+++ src/main/java/SingletonDesignPattern/SingletonDesign_V3.java
@@ -11,8 +11,8 @@ public class SingletonDesign_V3 {
 		this.age = age;
 	}
 
-	public static SingletonDesign_V3 setObject() {
-		SingletonDesign_V3 obj = new SingletonDesign_V3("abc", "ghana", 22);
+	public static SingletonDesign_V3 setObject(String name, String city, int age) {
+		SingletonDesign_V3 obj = new SingletonDesign_V3(name, city, age);
 		return obj;
 	}
 }
