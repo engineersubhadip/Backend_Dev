7138b29467fe0a86843bf0df4763be49684f8560
Subhadip Das <subhadipdas911@gmail.com>
2025-01-22 12:10:31 +0530

    Updated Client for as to why this is a shared resource

--- src/main/java/StaticDataMemberImplication/Client.java
+++ src/main/java/StaticDataMemberImplication/Client.java
@@ -16,6 +16,12 @@ public class Client {
 		System.out.println(c2.age); //25
 		System.out.println(Parent.age); //25
 		System.out.println(c1.age); //25
+		
+		/*
+		Since, age is an CLASS level property, this property will be present at CLASS LEVEL.
+		Meaning this is	A SHARED RESOURCE.
+		So, changing this property's value from an object will change at CLASS LEVEL OF PARENT, WHICH WILL BE REFLECTED ACROSS ALL ITS CHILD.
+		 */
 	}
 
 }
