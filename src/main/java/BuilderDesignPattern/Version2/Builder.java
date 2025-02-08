12a1394864bbbc57b0d0ee89f7c809b254d8e48c
Subhadip Das <subhadipdas911@gmail.com>
2025-02-08 12:14:06 +0530

    Moved all the validations from Student Constructor to getBuild()

--- src/main/java/BuilderDesignPattern/Version2/Builder.java
+++ src/main/java/BuilderDesignPattern/Version2/Builder.java
@@ -1,9 +1,27 @@
 package BuilderDesignPattern.Version2;
 
+import javax.naming.directory.InvalidAttributeValueException;
+
 public class Builder {
 	
 	int age;
 	String name;
 	int psp;
 	String email;
+	
+	public Student getBuild() throws InvalidAttributeValueException { // We have moved the validations from Student Constructor to getBuild() -> To maintain better SRP.
+		if (name == null || name.length() == 0) {
+			throw new InvalidAttributeValueException("Name cannot be null or empty");
+		}
+		if (age < 0 || age > 120) {
+			throw new InvalidAttributeValueException("Age should be between 1 and 120");
+		}
+		if (psp < 0 || psp > 100) {
+			throw new InvalidAttributeValueException("PSP should be between 1 and 100");
+		}
+		if (email == null || email.contains("@") == false) {
+			throw new InvalidAttributeValueException("Email cannot be null or it should contain @");
+		}
+		return new Student(new Builder());
+	}
 }
--- src/main/java/BuilderDesignPattern/Version2/Student.java
+++ src/main/java/BuilderDesignPattern/Version2/Student.java
@@ -11,24 +11,15 @@ public class Student {
 
 	public Student(Builder builder) throws InvalidAttributeValueException {
 
-		if (builder.name == null || builder.name.length() == 0) {
-			throw new InvalidAttributeValueException("Name cannot be null or empty");
-		}
 		this.name = builder.name;
-		if (builder.age < 0 || builder.age > 120) {
-			throw new InvalidAttributeValueException("Age should be between 1 and 120");
-		}
+
 		this.age = builder.age;
-		if (builder.psp < 0 || builder.psp > 100) {
-			throw new InvalidAttributeValueException("PSP should be between 1 and 100");
-		}
+
 		this.psp = builder.psp;
-		if (builder.email == null || builder.email.contains("@") == false) {
-			throw new InvalidAttributeValueException("Email cannot be null or it should contain @");
-		}
+
 		this.email = builder.email;
 	}
-	
+
 	public static Builder getBuilder() {
 		return new Builder();
 	}
