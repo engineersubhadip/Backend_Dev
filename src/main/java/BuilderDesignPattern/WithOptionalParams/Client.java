54430ee4454684736694435d23ee002bf8195afd
Subhadip Das <subhadipdas911@gmail.com>
2025-02-08 21:30:56 +0530

    How missing out on the mandatory fields we are unable to create Product Object only

--- src/main/java/BuilderDesignPattern/WithOptionalParams/Client.java
+++ src/main/java/BuilderDesignPattern/WithOptionalParams/Client.java
@@ -7,13 +7,13 @@ public class Client {
 //		Mandatory Fields :- Name, Price;
 //		Optional Fields :- Location, hashCode;
 		
-		/*
+		/* {Missing out on Price}
 		Product p = Product.getBuilder().setName("Hitachi").build();
 		System.out.println("Product Object: "+p); -> Unable to create object
 		We are getting an error here, it says please enter valid price;
 		*/
 		
-		/*
+		/* {Missing out on Name}
 		Product p = Product.getBuilder().setPrice(120).build();
 		System.out.println("Product Object: "+p);
 		We are getting error here, it says please enter valid name;
